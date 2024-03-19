21235d45d4722a29d6282177f2c346a83954d603
Aleksandr <aleksandr.jampolski@gmail.com>
2024-03-19 10:08:53 +0200

    Add player selection and button event listeners

--- app.js
+++ app.js
@@ -9,15 +9,29 @@ const gameData = {
 	outcome: 'Play a round!',
 };
 
+// Game buttons
+const buttons = document.querySelectorAll('.btn');
+
 // Randomly select computer choice
-function getComputerChoice() {
+function getComputerSelection() {
 	const choices = ['cockroach', 'foot', 'nuclear-bomb'];
 	const indexOfChoice = Math.floor(Math.random() * choices.length);
 	return choices[indexOfChoice];
 }
 
+// Get player selection
+function getPlayerSelection(e) {
+	gameData.playerSelection = e.currentTarget.dataset.value;
+	playRound(gameData.playerSelection, gameData.computerSelection);
+}
+
 // Play one round of the game
 function playRound(playerChoice, computerChoice) {}
+
+// Button click events
+buttons.forEach((button) =>
+	button.addEventListener('click', (e) => getPlayerSelection(e)),
+);
 
 // Game loop
 function playGame() {}
