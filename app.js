270f9d54b2e175937ff0630d4af7e4323864abdf
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2024-03-23 19:14:48 +0200

    Fix game, rewrite games logic form best of five into first to five

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // Store game data
 const gameData = {
-	playerScore: 0,
-	computerScore: 0,
-	playerSelection: '',
-	computerSelection: '',
-	currentRound: 0,
-	maxRound: 5,
-	outcome: 'Select A Hand To Play A Round!',
+  playerScore: 0,
+  computerScore: 0,
+  playerSelection: '',
+  computerSelection: '',
+  gameOver: false,
+  outcome: 'Select A Hand To Play A Round!',
 };
 
 // Game buttons
@@ -16,127 +15,121 @@ const showRules = document.querySelector('#show-rules');
 
 // Randomly select computer choice
 function getComputerSelection() {
-	const choices = ['cockroach', 'foot', 'nuclear-bomb'];
-	const indexOfChoice = Math.floor(Math.random() * choices.length);
-	return choices[indexOfChoice];
+  const choices = ['cockroach', 'foot', 'nuclear-bomb'];
+  const indexOfChoice = Math.floor(Math.random() * choices.length);
+  return choices[indexOfChoice];
 }
 
 // Play one round of the game
 function playRound(e) {
-	// End game if played max rounds
-	if (gameData.currentRound === gameData.maxRound) {
-		return;
-	}
+  // End game if played max rounds
+  if (gameData.playerScore === 5 || gameData.computerScore === 5) {
+    updateScreen();
+    return;
+  }
 
-	// Set player and computer selections
-	gameData.playerSelection = e.currentTarget.dataset.value;
-	gameData.computerSelection = getComputerSelection();
+  // Set player and computer selections
+  gameData.playerSelection = e.currentTarget.dataset.value;
+  gameData.computerSelection = getComputerSelection();
 
-	// Draw conditions
-	if (gameData.playerSelection === gameData.computerSelection) {
-		gameData.currentRound++;
-		gameData.outcome = "It's a Draw";
-		updateScreen();
-	} else if (
-		// Player win conditions
-		(gameData.playerSelection === 'cockroach' &&
-			gameData.computerSelection === 'nuclear-bomb') ||
-		(gameData.playerSelection === 'nuclear-bomb' &&
-			gameData.computerSelection === 'foot') ||
-		(gameData.playerSelection === 'foot' &&
-			gameData.computerSelection === 'cockroach')
-	) {
-		gameData.playerScore++;
-		gameData.currentRound++;
-		gameData.outcome = 'You win!';
-		updateScreen();
-	} else if (
-		// Computer win conditions
-		(gameData.computerSelection === 'cockroach' &&
-			gameData.playerSelection === 'nuclear-bomb') ||
-		(gameData.computerSelection === 'nuclear-bomb' &&
-			gameData.playerSelection === 'foot') ||
-		(gameData.computerSelection === 'foot' &&
-			gameData.playerSelection === 'cockroach')
-	) {
-		gameData.computerScore++;
-		gameData.currentRound++;
-		gameData.outcome = 'Computer wins!';
-		updateScreen();
-	}
+  // Draw conditions
+  if (gameData.playerSelection === gameData.computerSelection) {
+    gameData.outcome = "It's a Draw";
+    updateScreen();
+  } else if (
+    // Player win conditions
+    (gameData.playerSelection === 'cockroach' &&
+      gameData.computerSelection === 'nuclear-bomb') ||
+    (gameData.playerSelection === 'nuclear-bomb' &&
+      gameData.computerSelection === 'foot') ||
+    (gameData.playerSelection === 'foot' &&
+      gameData.computerSelection === 'cockroach')
+  ) {
+    gameData.playerScore++;
+    gameData.outcome = 'You win!';
+    updateScreen();
+  } else if (
+    // Computer win conditions
+    (gameData.computerSelection === 'cockroach' &&
+      gameData.playerSelection === 'nuclear-bomb') ||
+    (gameData.computerSelection === 'nuclear-bomb' &&
+      gameData.playerSelection === 'foot') ||
+    (gameData.computerSelection === 'foot' &&
+      gameData.playerSelection === 'cockroach')
+  ) {
+    gameData.computerScore++;
+    gameData.outcome = 'Computer wins!';
+    updateScreen();
+  }
 }
 
 // Update game UI
 function updateScreen() {
-	// Display the winner of the game
-	if (gameData.currentRound === gameData.maxRound) {
-		gameData.outcome = `Game is over, ${
-			gameData.playerScore > gameData.computerScore
-				? 'You won!'
-				: gameData.playerScore === gameData.computerScore
-				? "It's a Draw!"
-				: 'Computer won!'
-		} `;
-		restart.style.display = 'block';
-	}
+  // Display the winner of the game
+  if (gameData.playerScore === 5 || gameData.computerScore === 5) {
+    gameData.outcome = `Game is over, ${
+      gameData.playerScore > gameData.computerScore
+        ? 'You won!'
+        : gameData.playerScore === gameData.computerScore
+        ? "It's a Draw!"
+        : 'Computer won!'
+    } `;
+    restart.style.display = 'block';
+  }
 
-	// DOM variables
-	const outcome = document.querySelector('#outcome');
-	const playerSelectionText = document.querySelector('#player-selection');
-	const computerSelectionText = document.querySelector('#ai-selection');
-	const playerScoreText = document.querySelector('#player-score');
-	const computerScoreText = document.querySelector('#ai-score');
-	const progressBar = document.querySelector('#progress-bar');
+  // DOM variables
+  const outcome = document.querySelector('#outcome');
+  const playerSelectionText = document.querySelector('#player-selection');
+  const computerSelectionText = document.querySelector('#ai-selection');
+  const playerScoreText = document.querySelector('#player-score');
+  const computerScoreText = document.querySelector('#ai-score');
 
-	// Update the values based on gameData
-	outcome.textContent = gameData.outcome;
-	playerSelectionText.textContent = capitalizeFirstLetter(
-		gameData.playerSelection,
-	);
-	computerSelectionText.textContent = capitalizeFirstLetter(
-		gameData.computerSelection,
-	);
-	playerScoreText.textContent = gameData.playerScore;
-	computerScoreText.textContent = gameData.computerScore;
-	progressBar.dataset.value = `${gameData.currentRound}/${gameData.maxRound}`;
-	progressBar.value = gameData.currentRound;
-	progressBar.max = gameData.maxRound;
+  // Update the values based on gameData
+  outcome.textContent = gameData.outcome;
+  playerSelectionText.textContent = capitalizeFirstLetter(
+    gameData.playerSelection
+  );
+  computerSelectionText.textContent = capitalizeFirstLetter(
+    gameData.computerSelection
+  );
+  playerScoreText.textContent = gameData.playerScore;
+  computerScoreText.textContent = gameData.computerScore;
 }
 
 // Restart game
 function restartGame() {
-	// Hide button
-	restart.style.display = 'none';
+  // Hide button
+  restart.style.display = 'none';
 
-	// Reset values
-	(gameData.playerScore = 0),
-		(gameData.computerScore = 0),
-		(gameData.playerSelection = ''),
-		(gameData.computerSelection = ''),
-		(gameData.currentRound = 0),
-		(gameData.maxRound = 5),
-		(gameData.outcome = 'Select A Hand To Play A Round!'),
-		// Update screen
-		updateScreen();
+  // Reset values
+  (gameData.playerScore = 0),
+    (gameData.computerScore = 0),
+    (gameData.playerSelection = ''),
+    (gameData.computerSelection = ''),
+    (gameData.gameOver = false),
+    (gameData.outcome = 'Select A Hand To Play A Round!');
+
+  // Update screen
+  updateScreen();
 }
 
 // Display rules paragraph
 function toggleRules() {
-	const rulesText = document.querySelector('#rules');
-	// toggle show class
-	rulesText.classList.toggle('show');
+  const rulesText = document.querySelector('#rules');
+  // toggle show class
+  rulesText.classList.toggle('show');
 }
 
 // Helper function
 function capitalizeFirstLetter(string) {
-	const capitalLetter = string.slice(0, 1).toUpperCase();
-	const removedFirstLetter = string.slice(1);
-	return capitalLetter.concat(removedFirstLetter);
+  const capitalLetter = string.slice(0, 1).toUpperCase();
+  const removedFirstLetter = string.slice(1);
+  return capitalLetter.concat(removedFirstLetter);
 }
 
 // Button click events
 buttons.forEach((button) =>
-	button.addEventListener('click', (e) => playRound(e)),
+  button.addEventListener('click', (e) => playRound(e))
 );
 
 restart.addEventListener('click', restartGame);
